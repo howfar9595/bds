03fb47befad090f448b22c57dde74d2cbcbe77e4
Howfar <howfar.vip@qq.com>
2019-04-17 11:23:24 +0800

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,12 +1,12 @@
-# -*- coding:utf-8 -*-
-# 运行环境 Python3 , 将代码放在需要修改的快捷方式路径下执行
+# 运行环境 Python3
+from __future__ import print_function
 import os
 import time
 from itertools import permutations
 import progressbar  # 自行安装此Module
+import ctypes
 
 #
-# C:\Users\Administrator\Desktop> D:/Python36/python.exe 1.py
 # *注意事项*
 # 如果发现执行完成后，有部分快捷方式没有修改，请查看这些快捷方式是否在其他路径下。
 # 有个人用户桌面和公用桌面等路径的存在
@@ -19,15 +19,18 @@ lnks = []
 lnkNames = []
 p = progressbar.ProgressBar()
 
+path = os.path.expanduser('~/Desktop/')
+
 
 def getlnks():
-    files = os.listdir('.')
-    for file in files:
-        filename, type = os.path.splitext(file)
-        if type != '.lnk':
-            continue
-        lnks.append(file)
-        lnkNames.append(filename.encode('utf-8'))
+    paths = [os.path.expanduser('~/Desktop/'), 'C:/Users/Public/Desktop/']
+    for path in paths:
+        for file in os.listdir(path):
+            filename, type = os.path.splitext(file)
+            if type != '.lnk':
+                continue
+            lnks.append((path, file))
+            lnkNames.append(filename.encode('utf-8'))
 
 
 def change():
@@ -45,7 +48,8 @@ def change():
         except ValueError:
             continue
     p.start()
-    for i, file in enumerate(lnks):
+    for i, fileTuple in enumerate(lnks):
+
         p.update(i+1)
         time.sleep(0.1)
         for items in names:
@@ -55,21 +59,33 @@ def change():
             else:
                 names.remove(items)
                 lnkNames.append(items)
-                os.rename(file, items.decode('utf-8')+'.lnk')
+                os.rename(fileTuple[0] + fileTuple[1],
+                          fileTuple[0] + items.decode('utf-8')+'.lnk')
                 break
 
 
 def clear():
-    print('清空快捷方式图标', os.system(
+    print('Clear shortcut small arrow.', os.system(
         r'reg delete "HKEY_CLASSES_ROOT\lnkfile" /v IsShortCut /f'))
     time.sleep(1)
-    print('重启资源管理器', os.system(r'taskkill /f /im explorer.exe'))
+    print('Restart the resource manager.',
+          os.system(r'taskkill /f /im explorer.exe'))
     time.sleep(2)
     print(os.system('start explorer.exe'))
 
 
+def is_admin():
+    try:
+        return ctypes.windll.shell32.IsUserAnAdmin()
+    except:
+        return False
+
+
 if __name__ == '__main__':
-    getlnks()
-    change()
-    p.finish()
-    clear()
+    if is_admin():
+        getlnks()
+        change()
+        p.finish()
+        clear()
+    else:
+        print('Please run cmd in admin mode.')
